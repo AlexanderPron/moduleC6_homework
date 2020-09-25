5162e9f393319046761f9dcf93a3300fc159bc01
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-09-25 14:06:48 +0300

    Add files via upload

--- C6-3/c6-3.js
+++ C6-3/c6-3.js
@@ -1,1 +1,5 @@
-console.log('Hello, JS!');
+const btn = document.querySelector(".j-btn-test");
+btn.addEventListener("click", ()=>{
+    document.querySelector(".btn_icon_1").classList.toggle("hide");
+    document.querySelector(".btn_icon_2").classList.toggle("hide");
+});
