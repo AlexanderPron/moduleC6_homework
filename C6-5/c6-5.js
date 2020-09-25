b3f9b1a72afe2ca5f53564ff99cf5b22d5523d5f
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-09-25 17:48:39 +0300

    Add files via upload

--- C6-5/c6-5.js
+++ C6-5/c6-5.js
@@ -1,1 +1,12 @@
-console.log('Hello, JS!');
+let btn = document.querySelector(".j-btn-test");
+btn.addEventListener("click", () =>{
+    const sw = window.screen.width;
+    const sh = window.screen.height;
+    const ww = window.innerWidth;
+    const wh = window.innerHeight;
+    const wcw = document.documentElement.clientWidth;
+    const wch = document.documentElement.clientHeight;
+
+
+    alert(`Ширина экрана: ${sw}px \nВысота экрана: ${sh}px\nШирина окна: ${ww}px \nВысота окна: ${wh}px\nШирина экрана без учёта полосы прокрутки: ${wcw}\nВысота экрана без учёта полосы прокрутки: ${wch}`);
+});
