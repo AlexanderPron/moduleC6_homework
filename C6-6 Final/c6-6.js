1b84fa9d0a6135f252767c4b1f9dbfbc420dd0eb
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-10-01 00:25:34 +0300

    Add files via upload

--- C6-6 Final/c6-6.js
+++ C6-6 Final/c6-6.js
@@ -2,7 +2,7 @@ const sendBtn = document.querySelector("#sendBtn");
 const geoBtn = document.querySelector("#geoBtn");
 let currLogChat  = '';
 
-sendBtn.addEventListener("click", (e)=>{
+sendBtn.addEventListener("click", ()=>{
     const message = document.querySelector(".msg").value;
     if (message){
         const websocket = new WebSocket('wss://echo.websocket.org/');
@@ -20,12 +20,25 @@ sendBtn.addEventListener("click", (e)=>{
     }
 });
     
-
-geoBtn.addEventListener("click", (e)=>{
+geoBtn.addEventListener("click", ()=>{
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
-          const { coords } = position;
-          alert(coords.latitude+'  '+coords.longitude);
+            const { coords } = position;
+            const websocket = new WebSocket('wss://echo.websocket.org/');
+            websocket.onopen = function(evt){
+                websocket.send(coords);
+            }
+            websocket.onmessage = function(evt){
+                currLogChat += `<div class = "client-request"><a class = "geoLink" href = "https://www.openstreetmap.org/#map=15/${evt.data.latitude}/${evt.data.longitude}" target="_blank">Я тут</a></div>`;
+                document.querySelector(".dialog-div").innerHTML = currLogChat;
+                websocket.close;
+            }
+        },
+        ()=>{
+            alert('Невозможно получить ваше местоположение');
         });
+    }
+    else{
+        alert("Ваш браузер не поддерживается");
     }
 });
