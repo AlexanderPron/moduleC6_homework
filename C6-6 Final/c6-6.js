7a2d8ecd93049b72854fbbd1cf9ce5ec4434f303
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-09-30 17:43:10 +0300

    Add files via upload

--- C6-6 Final/c6-6.js
+++ C6-6 Final/c6-6.js
@@ -1,1 +1,31 @@
-console.log('Hello, JS!');
+const sendBtn = document.querySelector("#sendBtn");
+const geoBtn = document.querySelector("#geoBtn");
+let currLogChat  = '';
+
+sendBtn.addEventListener("click", (e)=>{
+    const message = document.querySelector(".msg").value;
+    if (message){
+        const websocket = new WebSocket('wss://echo.websocket.org/');
+        currLogChat += `<div class = "client-request">${message}</div>`;
+        document.querySelector(".dialog-div").innerHTML = currLogChat;
+        websocket.onopen = function(evt){
+            websocket.send(message);
+        }
+        websocket.onmessage = function(evt){
+            currLogChat += `<div class = "server-answer">${evt.data}</div>`;
+            document.querySelector(".dialog-div").innerHTML = currLogChat;
+            websocket.close;
+        }
+        document.querySelector(".msg").value = '';
+    }
+});
+    
+
+geoBtn.addEventListener("click", (e)=>{
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { coords } = position;
+          alert(coords.latitude+'  '+coords.longitude);
+        });
+    }
+});
